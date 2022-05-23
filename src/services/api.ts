795fec2e2ea3b50b716777a76fc545acf45dab37
guilherssousa <guilherssousa@gmail.com>
2022-05-23 01:47:06 -0300

    change cors policy

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,5 @@ import axios from "axios";
 const isDev = process.env.NODE_ENV === "development";
 
 export default axios.create({
-  baseURL: isDev
-    ? "http://localhost:1073/api/"
-    : "https://correioanonimo.com.br/api/",
+  baseURL: isDev ? "http://localhost:1073/api/" : "/api/",
 });
